9c32f054595114ad387ac54fc5820528c3909e72
mickisbrown10 <mickisbrown10@gmail.com>
2025-07-10 22:53:02 +0000

    about modal has all of the information in it now.

--- components/about-modal-content.jsx
+++ components/about-modal-content.jsx
@@ -1,6 +1,98 @@
+import './about-modal-content.css'
+import collegePicture from '../src/assets/cu.png'
+import myPicture from '../src/assets/me.jpg'
+import { useState } from 'react'
+
 function AboutModal() {
+    const [isSkills, setIsSkills] = useState(true)
+    const myInfo = {
+        "picture": myPicture,
+        "name": "Mickey Brown",
+        "description": "Hi! Im a 26 year old software engineer interested in software development, sports, and finances!"
+    }
+
     return (
-        <p>About!</p>
+        <>
+            <div className="modal-content-about">
+                <div className="modal-left">
+                    <img src={myInfo["picture"]} style={{width: '100%', height: '20%'}}/>
+                    <p>{myInfo["name"]}</p>
+                    <p>{myInfo["description"]}</p>
+                </div>
+                <div className="modal-right">
+                    <div style={{display: 'flex'}}>
+                        <div className="education-content">
+                            <div className='content-header'>
+                                <p>Education</p>
+                            </div>
+                            <div style={{display: 'flex', flex: '2', margin: '5px'}}>
+                                <div>
+                                    <p>BS in Electrical and Computer Engineering</p>
+                                    <p>university of colorado</p>
+                                    <p>2018-2022</p>
+                                </div>
+                                <img src={collegePicture} style={{width: '50px', height: '50px'}}/>
+                            </div>
+                        </div>
+                        <div className="skills-content">
+                            {isSkills ? 
+                            <>
+                                <div className='content-header'>
+                                    <p>Skills</p>
+                                </div>
+                                <div className='content-body'>
+                                    <button onClick={() => setIsSkills(!isSkills)}>x</button>
+                                    <p>Python</p>
+                                    <p>Javascript</p>
+                                    <p>C/C++</p>
+                                    <p>Elixir</p>
+                                </div>
+                            </>
+                            : 
+                            <>
+                                <div className='content-header'>
+                                    <p>Software</p>
+                                </div>
+                                <div className='content-body'>
+                                    <button onClick={() => setIsSkills(!isSkills)}>x</button>
+                                    <p>Github</p>
+                                    <p>Insomnia</p>
+                                    <p>VS Code</p>
+                                </div>
+                            </>
+                            }
+                            
+                        </div>
+                    </div>
+                    <div className="experiences-content">
+                        <div className='content-header'>
+                            <p>Experiences</p>
+                        </div>
+                        <div className='content-body'>
+                            <div className='experience-line'>
+                                <div className='experience-left'>
+                                    <p>Software Engineer</p>
+                                    <p>Podium</p>
+                                </div>
+                                <div className='experience-right'>
+                                    <p>Jun 2022 - Dec 2022</p>
+                                </div>
+                            </div>
+                            <div className='experience-line'>
+                                <div className='experience-left'>
+                                    <p>Teacher</p>
+                                    <p>DPS</p>
+                                </div>
+                                <div className='experience-right'>
+                                    <p>Jan 2024 - Present</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
+
 export default AboutModal
--- components/projects-modal-content.jsx
+++ components/projects-modal-content.jsx
@@ -1,6 +1,42 @@
+import { useState } from 'react'
+import './projects-modal-content.css'
+
 function ProjectsModal() {
+    const [index, setIndex] = useState(0)
+    const projects = {
+        "0": {
+            "title": "Budget Tracker",
+            "body": "Body",
+            "link": "link"
+        },
+        "1": {
+            "title": "This portfolio!",
+            "body": "Body",
+            "link": "link"
+        },
+    }
+
+    function handleLeftArrow() {
+        if (index > 0) {
+            setIndex(index - 1)
+        }
+    }
+    function handleRightArrow() {
+        if (index < Object.keys(projects).length - 1) {
+            setIndex(index + 1)
+        }
+    }
+
     return (
-        <p>Projects</p>
+        <div className='modal-content-projects'>
+            <button onClick={handleLeftArrow} style={{marginLeft: '1%'}}>{"<"}</button>
+            <div className='project-card'>
+                <p>{projects[index]["title"]}</p>
+                <p>{projects[index]["body"]}</p>
+                <a href={projects[index]["link"]}>GitHub Link</a>
+            </div>
+            <button onClick={handleRightArrow} style={{marginRight: '1%'}}>{">"}</button>
+        </div>
     )
 }
 export default ProjectsModal
