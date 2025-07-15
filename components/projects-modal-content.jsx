cbb9e5d1791a0a74192944c8cc5f5fdbf3b47599
mickisbrown10 <mickisbrown10@gmail.com>
2025-07-15 23:24:50 +0000

    added more stuff to the projects tab. Hopefully more interesting there.

--- components/projects-modal-content.jsx
+++ components/projects-modal-content.jsx
@@ -7,13 +7,15 @@ function ProjectsModal() {
         "0": {
             "title": "This portfolio!",
             "body": "This project can best be described as a clever way to present my resume and technical skills, using a computer simlation web app.",
+            "learned": "The biggest thing I have learned from this project would be the the importance of designing and planning data structures. While that might be something that sounds obvious in the realm of development, this project really showed me how powerful making a look up object might be and the need for organization when passing all that information around the application.",
             "skills": ["React", "CSS", "HTML", "JavaScript"],
             "link": "https://github.com/MickeyBrow/my-portfolio"
         },
         "1": {
             "title": "Budget Tracker",
             "body": "This was one of my first complex personal projects that I built. It was more of a learning experience than anything, giving me the chance to learn how to plan, construct, and deploy a web app by myself!",
-            "skills": ["NextJs", "CSS", "HTML", "JavaScript", "Python", "Flask"],
+            "learned": "With this being my first project, there is a ton I learned through the experience. However, the biggest one that comes to mind is confidence. This project was built right after my employment at Podium so it was an opportunity to put all of the skills that I had just learned to the test.",
+            "skills": ["NextJs", "CSS", "HTML", "JavaScript", "Python", "Flask", "Firebase"],
             "link": "https://github.com/MickeyBrow/budget-tracker"
         },
     }
@@ -40,13 +42,14 @@ function ProjectsModal() {
                         <p style={{flex: 1, textAlign: 'center'}}>{projects[index]["body"]}</p>
                     </div>
                     <div className='project-learned'>
-                        <p>What I learned:</p>
+                        <p style={{fontWeight: 'bolder', textDecoration: 'underline'}}>What I learned:</p>
+                        <p style={{textAlign: 'center'}}>{projects[index]["learned"]}</p>
                     </div>
                     <div className='project-programs'>
-                        <p>Languages & Programs used:</p>
+                        <p style={{fontWeight: 'bolder', textDecoration: 'underline'}}>Languages & Programs used:</p>
                         <ul>
                             {projects[index]["skills"].map((skill) => (
-                                <li key={skill} style={{display: 'inline-block', marginRight: '5%'}}>{skill}</li>
+                                <li key={skill} style={{marginRight: '5%'}}>{skill}</li>
                             ))}
                         </ul>
                     </div>
